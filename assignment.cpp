df9722da1bb65f8e5fe87e58f121b8b05201f3fb
kuansiing <chinkuansiing@gmail.com>
2024-12-20 22:36:06 +0800

    hi

--- assignment.cpp
+++ assignment.cpp
@@ -17,18 +17,18 @@ string outputFileName = "outputfile1.txt";
 ofstream outputFile(outputFileName);
 
 void create_output_screen_and_file() {
-   
-    
+
+
     if (!outputFile.is_open()) {
         cout << "Unable to create output file" << endl;
         return;
     }
-    
+
     cout << "> CREATE " << outputFileName << ";" << endl;
     outputFile << "> CREATE " << outputFileName << ";" << endl;
-    
 
-    
+
+
 }
 
 void create_database() {
@@ -45,8 +45,31 @@ void insert_into_table() {
 }
 
 void select_all_from_table_in_csv_mode() {
-    // Implementation here
+
+    // Define sample table data using vectors
+    vector<int> customer_id = {1, 2, 3, 4};
+    vector<string> customer_name = {"name1", "name2", "name3", "name4"};
+    vector<string> customer_city = {"city1", "city2", "city3", "city4"};
+    vector<string> customer_state = {"state1", "state2", "state3", "state4"};
+    vector<string> customer_country = {"country1", "country2", "country3", "country4"};
+    vector<string> customer_phone = {"phone1", "phone2", "phone3", "phone4"};
+    vector<string> customer_email = {"email1", "email2", "email3", "email4"};
+
+    // Print table header in CSV format
+    cout << "customer_id,customer_name,customer_city,customer_state,customer_country,customer_phone,customer_email\n";
+    outputFile << "customer_id,customer_name,customer_city,customer_state,customer_country,customer_phone,customer_email\n";
+
+    // Print table data in CSV format
+    for (size_t i = 0; i < customer_id.size(); i++) {
+        cout << customer_id[i] << "," << customer_name[i] << "," << customer_city[i] << ","
+             << customer_state[i] << "," << customer_country[i] << "," << customer_phone[i] << ","
+             << customer_email[i] << "\n";
+        outputFile << customer_id[i] << "," << customer_name[i] << "," << customer_city[i] << ","
+                   << customer_state[i] << "," << customer_country[i] << "," << customer_phone[i] << ","
+                   << customer_email[i] << "\n";
+    }
 }
+
 
 int main() {
     fstream fileInput;
@@ -67,7 +90,7 @@ int main() {
     while (getline(fileInput, line)) {
         cout << line << endl;
     }
-    
+
 
     create_output_screen_and_file();
     create_database();
@@ -77,11 +100,11 @@ int main() {
     outputFile.close();
 
 
-    
+
 
     //fileOutputName = "fileOutput1.txt"; //incorrect
     //fileInput.close(); CREATE fileOutputName << "";
-    
+
     cout << "CREATE" << endl;
     cout << "DATABASES" << endl;
 
